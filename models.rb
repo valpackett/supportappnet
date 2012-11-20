3dcbb1c4233a51adc15851ef14922e8a12e41b19
myfreeweb <floatboth@me.com>
2012-11-20 19:39:47 +0400

    add mongolab support

--- models.rb
+++ models.rb
@@ -1,10 +1,21 @@
 require 'curator'
+require 'uri'
 require 'mongo'
+
+mongolab_uri = ENV['MONGOLAB_URI']
+unless mongolab_uri.nil?
+  uri  = URI.parse mongolab_uri
+  conn = Mongo::Connection.from_uri mongolab_uri
+  db   = uri.path.gsub /^\//, ''
+else
+  conn = Mongo::Connection.new
+  db   = "supportadn"
+end
 
 Curator.configure(:mongo) do |config|
   config.environment = "development"
-  config.client      = Mongo::Connection.new
-  config.database    = "supportadn"
+  config.client      = conn
+  config.database    = db
   config.migrations_path = File.expand_path(File.dirname(__FILE__) + "/migrate")
 end
 
