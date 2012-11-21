8c45476ea074dab76c98c1200f690ea2ea2e621e
myfreeweb <floatboth@me.com>
2012-11-21 19:33:34 +0400

    me condition in permissions

--- app/permissions.rb
+++ app/permissions.rb
@@ -1,17 +1,17 @@
 module Permissions
   def owns_page?
-    @page.author_adn_id == @me['id']
+    !@me.nil? && @page.author_adn_id == @me['id']
   end
 
   def can_vote?(entry)
-    entry['user']['id'] != @me['id']
+    !@me.nil? && entry['user']['id'] != @me['id']
   end
 
   def can_archive_suggestion?
-    @page.author_adn_id == @me['id']
+    !@me.nil? && @page.author_adn_id == @me['id']
   end
 
   def can_delete_suggestion?
-    @entry['user']['id'] == @me['id']
+    !@me.nil? && @entry['user']['id'] == @me['id']
   end
 end
