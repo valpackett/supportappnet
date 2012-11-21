74767576e3fc2a09c2993da04b4f0a9ce94a1d41
myfreeweb <floatboth@me.com>
2012-11-21 17:59:11 +0400

    add separate helpers file

--- app/app.rb
+++ app/app.rb
@@ -9,6 +9,7 @@ require 'nokogiri'
 require_relative 'oauth2.rb'
 require_relative 'models.rb'
 require_relative 'validator.rb'
+require_relative 'helpers.rb'
 
 enable :sessions
 set :session_secret, ENV['SECRET_KEY'] || 'aaaaa'
@@ -36,48 +37,6 @@ end
 
 not_found do
   slim :not_found
-end
-
-helpers do
-  def unmention(post)
-    df = Nokogiri::HTML.fragment(post)
-    first_mention = df.css('[itemprop=mention]').first
-    first_mention.unlink unless first_mention.nil?
-    first_hashtag = df.css('[itemprop=hashtag]').first
-    first_hashtag.unlink unless first_hashtag.nil?
-    df.to_html
-  end
-
-  def dateformat(datestr)
-    Time.parse(datestr).strftime '%B %d, %Y %R'
-  end
-
-  def active_filter?(type)
-    params[:filter] == type.downcase
-  end
-
-  def types
-    [
-      {:slug => 'ideas',  :singular => 'Idea',   :plural => 'Ideas',  :icon => 'icon-bullhorn'},
-      {:slug => 'bugs',   :singular => 'Bug',    :plural => 'Bugs',   :icon => 'icon-fire'},
-      {:slug => 'praise', :singular => 'Praise', :plural => 'Praise', :icon => 'icon-heart'},
-    ]
-  end
-
-  def type_of_entry(entry)
-    anns = entry['annotations'].select { |a| a['type'] == 'com.floatboth.supportadn.entry' }
-    types.select { |t| t[:slug] == anns.first['value']['type'] }.first unless anns.empty?
-  end
-
-  def get_last_form
-    form = session[:form] || {}
-    session[:form] = nil
-    form
-  end
-
-  def id_to_name(id)
-    @adn.get("users/#{id}").body['data']['username']
-  end
 end
 
 get '/auth/appdotnet/callback' do
--- spec/validator_spec.rb
+++ spec/validator_spec.rb
@@ -13,4 +13,10 @@ describe Validator do
     Validator.page_repo.should_receive(:find_first_by_name).with("exists").and_return(true)
     lambda { Validator.valid_page?("exists", "a") }.should raise_error ValidationException
   end
+
+  it "validates posts" do
+    lambda { Validator.valid_post?("", "") }.should raise_error ValidationException
+    lambda { Validator.valid_post?("a" * 257, "a" * 257) }.should raise_error ValidationException
+    Validator.valid_post?("@myfreeweb hello", "hello").should be true
+  end
 end
